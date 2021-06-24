4292015c6d952c167d7aa08c814da88527289667
Colin Rofls <colin@cmyr.net>
2021-06-24 11:42:14 -0400

    Impl for tuples

--- keypath/src/impls.rs
+++ keypath/src/impls.rs
@@ -90,6 +90,176 @@ keyable_leaf!(f64);
 
 keyable_leaf!(String);
 
+macro_rules! tuple_impls {
+    ($(
+        $Tuple:ident {
+            $(($idx:tt) -> $T:ident)+
+        }
+    )+) => {
+        $(
+            impl<$($T: RawKeyable),+> RawKeyable for ($($T,)+) {
+                fn as_any(&self) -> &dyn Any {
+                    self
+                }
+                fn as_any_mut(&mut self) -> &mut dyn Any {
+                    self
+                }
+                fn get_field(&self, ident: &[PathComponent]) -> Result<&dyn RawKeyable, FieldError> {
+                    match ident.split_first() {
+                        None => Ok(self),
+                        $( Some((PathComponent::Unnamed($idx), rest)) => self.$idx.get_field(rest),)+
+                        Some((head, rest)) => {
+                            Err(FieldErrorKind::InvalidField(head.to_owned())
+                                .into_error(self, rest.len()))
+                        }
+                    }
+                }
+
+                fn get_field_mut(
+                    &mut self,
+                    ident: &[PathComponent],
+                ) -> Result<&mut dyn RawKeyable, FieldError> {
+                    match ident.split_first() {
+                        None => Ok(self),
+                        $( Some((PathComponent::Unnamed($idx), rest)) => self.$idx.get_field_mut(rest),)+
+                        Some((head, rest)) => {
+                            Err(FieldErrorKind::InvalidField(head.to_owned())
+                                .into_error(self, rest.len()))
+                        }
+                    }
+                }
+            }
+
+            pub struct $Tuple<$($T),+>($(PhantomData<$T>),+);
+
+
+            impl<$($T),+> $Tuple<$($T,)+> {
+                pub const fn new() -> Self {
+                    $Tuple($( make_phantom::<$T>() ),+)
+                }
+            }
+
+            impl<$($T: RawKeyable),+> Keyable for ($($T,)+) {
+                type Mirror = $Tuple<$($T),+>;
+                fn mirror() -> Self::Mirror {
+                    $Tuple::<$($T),+>::new()
+                }
+
+            }
+
+
+        )+
+    }
+}
+
+const fn make_phantom<T>() -> PhantomData<T> {
+    PhantomData
+}
+tuple_impls! {
+    Tuple1 {
+        (0) -> A
+    }
+    Tuple2 {
+        (0) -> A
+        (1) -> B
+    }
+    Tuple3 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+    }
+    Tuple4 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+    }
+    Tuple5 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+    }
+    Tuple6 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+    }
+    Tuple7 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+    }
+    Tuple8 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+    }
+    Tuple9 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+    }
+    Tuple10 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+    }
+    Tuple11 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+    }
+    Tuple12 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+        (11) -> L
+    }
+}
+
 impl<T: RawKeyable> RawKeyable for Vec<T> {
     fn as_any(&self) -> &dyn Any {
         self
--- keypath/tests/tuple_struct.rs
+++ keypath/tests/tuple_struct.rs
@@ -1,14 +1,14 @@
 use keypath::{keypath, Keyable};
 
 #[derive(Keyable)]
-pub struct DemoPerson(String, f64, Size);
+pub struct DemoPerson(String, f64, Size, (f32, f32));
 
 #[derive(Keyable)]
 pub struct Size(f64, f64);
 
 #[test]
 fn keypath() {
-    let mut person = DemoPerson("coco".to_string(), 42.0, Size(1.0, 5.0));
+    let mut person = DemoPerson("coco".to_string(), 42.0, Size(1.0, 5.0), (2.0, 4.0));
 
     let path = keypath!(DemoPerson.2 .0);
     assert_eq!(person[&path], 1.0);
